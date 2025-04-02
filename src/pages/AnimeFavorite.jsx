b317017114314128167935f949813f49e41d9988
shoya-m1 <ilmimusthofa8189@gamil.com>
2025-04-02 17:19:32 +0700

    update ui animeFavorite

--- src/pages/AnimeFavorite.jsx
+++ src/pages/AnimeFavorite.jsx
@@ -6,10 +6,12 @@ const AnimeFavorite = () => {
   const { favoriteAnimes } = useContext(FavoritesContext);
   return (
     <main className="md:pt-26 pt-20 min-h-screen">
-      <div className="lg:m-auto px-2 h-full mx-2 py-5 max-w-6xl bg-neutral-900 border border-1 border-neutral-700/60 rounded-xl">
-        <section className="w-full overflow-hidden m-auto h-10 md:h-12 bg-neutral-700 rounded-md flex justify-center items-center">
+      <div className="lg:m-auto h-full mx-2 py-5 max-w-6xl bg-neutral-900 border border-1 border-neutral-700/60 rounded-xl">
+        <div className="px-2">
+        <div className="w-full overflow-hidden h-10 md:h-12 bg-neutral-700 rounded-md flex justify-center items-center">
           <h1 className="font-bold md:text-xl text-elipsis line-clamp-1">Anime Favorite</h1>
-        </section>
+        </div>
+        </div>
 
         <div className="flex flex-wrap gap-2 justify-center items-center mt-5">
           {favoriteAnimes.length > 0 ? favoriteAnimes.map((anime, i) => <CardAnime key={i} {...anime} />) : <p className="text-neutral-200 text-lg font-semibold"> Data tidak ditemukan</p>}
